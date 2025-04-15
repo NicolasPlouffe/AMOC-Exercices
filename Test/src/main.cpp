91172d9cd8942fe4b5ec20ce89aae83efd5963a9
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-04-15 16:34:17 -0400

    UpdateDEpot

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -7,6 +7,18 @@
 Alphabet Alphabet;
 
 void setup() {
+  Serial.end();
+  Serial.begin(9600);
+
+  int valeur1 =42;
+  int valeur2 =13;
+
+  Serial.println(42);
+  Serial.println("Bonjour à tous !");
+  Serial.println(String(42, HEX));
+  Serial.println("0x" + String(42, HEX));
+  Serial.println("int valeur1 (int * : 0x" + String((uint16_t)(&valeur1), HEX) + ") = " + String(valeur1));
+  Serial.println("int valeur2 (int * : 0x" + String((uint16_t)(&valeur2), HEX) + ") = " + String(valeur2));
 }
 
 void loop() {
