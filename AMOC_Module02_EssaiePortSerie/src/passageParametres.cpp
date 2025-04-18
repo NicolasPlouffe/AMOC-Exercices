38a1e5ae36bd3343c878bba48e439298d1a39037
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-04-18 15:34:08 -0400

    Update

--- AMOC_Module02_EssaiePortSerie/src/passageParametres.cpp
+++ AMOC_Module02_EssaiePortSerie/src/passageParametres.cpp
@@ -9,14 +9,15 @@ void passageParametres1(int p_v1, int &p_r1, int *p_p1) {
     Serial.println("int *p_p1 (int * : 0x" + String((uint16_t)(&p_p1), HEX) + ") adresse : 0x" + String((uint16_t)p_p1,HEX) + " = " + String(*p_p1));
 }
 
-void passageParametres2(int &p_v1, int **p_p1){
+void passageParametres2(int*& p_r1, int** p_pp1){
 
-    int* nouvelEntier1 = new int(33);
-    int* nouvelEntier2 = new int(404);
-    
     // modifie le pointeur &p_v1 en créant un nouvel entier sur le tas
-    &p_v1 = nouvelEntier;
-
+    p_r1 = new int(33);
+    Serial.println("La valeure de int &p_r1(int * 0x" + String((uint16_t)(&p_r1), HEX) + ") = " + String((uintptr_t)p_r1, HEX));
+    
     // modifie le pointeur **p_p1 pointé en créant un nouvel entier sur le tas
+    *p_pp1 = new int(404);
+    Serial.println("La valeure de int **p_pp1(int * 0x" + String((uint16_t)(**p_pp1), HEX) + ") = " + String(**p_pp1));
+
 }
 
