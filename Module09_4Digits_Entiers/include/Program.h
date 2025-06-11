13b475d1e2859f6a4dd979ef4b317c82460220f0
NicolasPlouffe <nicolas.plouffe.qc.ca@gmail.com>
2025-06-10 21:42:20 -0400

    MAJ

--- Module09_4Digits_Entiers/include/Program.h
+++ Module09_4Digits_Entiers/include/Program.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <Arduino.h>
 
-#include "Affichage4Digits.h";
+#include "Affichage4Digits.h"
 
 class Program {
 
--- Module09_4Digits_Entiers/src/Program.cpp
+++ Module09_4Digits_Entiers/src/Program.cpp
@@ -4,28 +4,29 @@
 
 #include "Affichage4DigitsProxyTM1637.h"
 
+#include "Chronometre.h"
+
 Program::Program()  {
     this->m_affichage4Digits =
         new Affichage4Digits(new Affichage4DigitsProxyTM1637(2, 3));
 }
 
+Chronometre chronometre = Chronometre();
+
 void Program::loop() {
-    /*static uint16_t compteur = 0;
+    chronometre.Tick();
 
-    // Afficher alternance compteur/CACA
-    if(compteur % 2 == 0) {
-        m_affichage4Digits->AfficherEntier(compteur);
-    } else {
 
-    }*/
+
     m_affichage4Digits->afficher(
-               Affichage4Digits::segments[12] , // !
-               Affichage4Digits::segments[0] , // F
-               Affichage4Digits::segments[12] | 0x80 , // 0
-               Affichage4Digits::segments[17] | 0x80 // U
-           );
-    /*compteur++;
-    if(compteur > 9999) compteur = 0;*/
+        m_affichage4Digits->segments[chronometre.getDizaineHeure()],
+         m_affichage4Digits->segments[chronometre.getUniteHeure()] | 0x80,
+         m_affichage4Digits->segments[chronometre.getDizaineMinute()],
+         m_affichage4Digits->segments[chronometre.getUniteMinute()]
+    );
 
-    delay(500);
+
+    /*
+    m_affichage4Digits->AfficherEntier(chronometre.getCompteur());
+*/
 }
